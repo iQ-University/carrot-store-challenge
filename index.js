55862794d5435c3d8cdf8493924ad2a8be9b89cc
Marian Dandarila <marian_dandarila@gmail.com>
2020-03-05 17:58:42 +0200

    append

--- index.js
+++ index.js
@@ -3,15 +3,33 @@ fetch('http://localhost:3000/products')
     return response.json();
   })
   .then((data) => {
-    
     let products = data;
     console.log(products)
 
-    for(i=0; i < products.length; i++){
-      document.getElementById("img").src = products[0].imgUrl ;
-      document.getElementById("name").innerHTML = products[0].name; 
-    }
-  });
- 
 
+for(i=0; i < products.length; i++) {
+
+        let cards = document.getElementsByClassName("card")[i];
+        let p = document.createElement("p");
+        p.innerHTML = products[i].name;
+        let img = document.createElement("img");
+        img.src = products[i].imgUrl;
+        cards.appendChild(img);
+        cards.appendChild(p);
+        }
+
+
+
+    // for(i=0; i < products.length; i++) {
+
+    //     let cards = document.getElementsByClassName("card")[i];
+    //     let p = document.createElement("p");
+    //     p.innerHTML = products[i].name;
+    //     let img = document.createElement("img");
+    //     img.src = products[i].imgUrl;
+    //     cards.appendChild(img);
+    //     cards.appendChild(p);
+    //     }
+  }); 
+    
 
