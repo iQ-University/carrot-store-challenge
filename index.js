0f09a31d44d4eaacde3c6c939d51395f068500e6
Marian Dandarila <marian_dandarila@yahoo.com>
2020-03-05 21:51:21 +0200

    api response

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ fetch('http://localhost:3000/products')
     let products = data;
     console.log(products)
 
-
-for(i=0; i < products.length; i++) {
-
-        let cards = document.getElementsByClassName("card")[i];
-        let p = document.createElement("p");
-        p.innerHTML = products[i].name;
-        let img = document.createElement("img");
-        img.src = products[i].imgUrl;
-        cards.appendChild(img);
-        cards.appendChild(p);
+    const container = document.getElementById('cardsContainer');
+    let html = '';
+    for(i=0; i < products.length; i++) {
+        html += ('<div class="card"><p>' + products[i].name + '</p></div>');
+        container.innerHTML = html;
         }
 
 
