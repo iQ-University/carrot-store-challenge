a37feb16b6dcc0ec74311b059fa66874477153b7
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-03-05 14:03:06 +0200

    displaying text from server using .innerHTML

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,17 @@ fetch("http://localhost:3000/products")
     return response.json();
   })
   .then(data => (myData = data))
-  .then(() => console.log(myData, "aici"));
+  .then(() => console.log(myData, "aici"))
+  .then(() => {
+    //product data
+    document.getElementById("prod-description").innerHTML =
+      myData[0].description;
+    document.getElementById("prod-title").innerHTML = myData[0].name;
+    document.getElementById("prod-image").src = myData[0].imgUrl;
+    //comments data
+    document.getElementById("comm-text").innerHTML =
+      myData[0].comments[0].comment;
+    document.getElementById("comm-name").innerHTML = myData[0].comments[0].user;
+    document.getElementById("comm-image").src =
+      myData[0].comments[0].userImgUrl;
+  });
