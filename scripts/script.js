9e212b8961383e6c2d4c23817d084c233c548aed
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-03-05 13:21:48 +0200

    fetching data from server

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-console.log("online");
+fetch("http://localhost:3000/products")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => (myData = data))
+  .then(() => console.log(myData, "aici"));
