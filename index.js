cc2d80b694ba32d31c34d44ad72dc0dc9d6b77ee
Marian Dandarila <marian_dandarila@yahoo.com>
2020-03-08 17:06:52 +0200

    css fixed

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-fetch('http://localhost:3000/products')
+
+const apiRoot = 'http://localhost:3000/products';
+
+fetch(apiRoot)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     let products = data;
-    console.log(products)
-
+    // console.log(products);
     const container = document.getElementById('cardsContainer');
     let html = '';
     for(i=0; i < products.length; i++) {
-        html += ('<div class="card"><p>' + products[i].name + '</p></div>');
+        html += ('<div class="card"><img src = " ' + products[i].imgUrl + ' "> </img> <p>' + products[i].name + '</p> <button class ="goToDetails"><a href="">More</a></button></div>');
         container.innerHTML = html;
-        }
-
-
-
-    // for(i=0; i < products.length; i++) {
-
-    //     let cards = document.getElementsByClassName("card")[i];
-    //     let p = document.createElement("p");
-    //     p.innerHTML = products[i].name;
-    //     let img = document.createElement("img");
-    //     img.src = products[i].imgUrl;
-    //     cards.appendChild(img);
-    //     cards.appendChild(p);
-    //     }
+      }
   }); 
     
 
