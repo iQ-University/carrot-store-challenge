7284f81607e974309ed5749cf49e64a067d5474c
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-03-16 11:24:58 +0200

    implementing id recognition function on buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+// no button id recognition yet
+// let clickedId = 0;
+// localStorage.removeItem("clickedId", JSON.stringify(clickedId));
+
+getButtonId = idVal => {
+  let clickedId = idVal;
+  localStorage.setItem("clickedId", JSON.stringify(clickedId));
+};
+
 fetch("http://localhost:3000/products")
   .then(response => {
     return response.json();
@@ -5,15 +14,22 @@ fetch("http://localhost:3000/products")
   .then(data => (myData = data))
   .then(() => console.log(myData, "aici"))
   .then(() => {
-    //product data
-    document.getElementById("prod-description").innerHTML =
-      myData[0].description;
-    document.getElementById("prod-title").innerHTML = myData[0].name;
-    document.getElementById("prod-image").src = myData[0].imgUrl;
-    //comments data
-    document.getElementById("comm-text").innerHTML =
-      myData[0].comments[0].comment;
-    document.getElementById("comm-name").innerHTML = myData[0].comments[0].user;
-    document.getElementById("comm-image").src =
-      myData[0].comments[0].userImgUrl;
+    for (let i = 0; i <= myData.length; i++) {
+      if (myData[i].id == clickedId) {
+        //product data
+        document.getElementById("prod-title").innerHTML = myData[i].name;
+        document.getElementById("prod-description").innerHTML =
+          myData[i].description;
+        document.getElementById("prod-image").src = myData[i].imgUrl;
+        //comments data
+        for (let j = 0; j <= myData[i].comments.length; j++) {
+          document.getElementById("comm-user").innerHTML =
+            myData[i].comments[j].user;
+          document.getElementById("comm-text").innerHTML =
+            myData[i].comments[j].comment;
+          document.getElementById("comm-image").innerHTML =
+            myData[i].comments[j].userImgUrl;
+        }
+      }
+    }
   });
