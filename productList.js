80dc07a73826eedad05a49b1e2beae74fb91a754
Marian Dandarila <marian_dandarila@yahoo.com>
2020-03-17 18:14:08 +0200

    details js fixed but with a second API call

---
+++ productList.js
@@ -0,0 +1,30 @@
+const apiRoot = 'http://localhost:3000/products';
+
+fetch(apiRoot)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let products = data;
+
+    console.log(products);
+    const container = document.getElementById('cardsContainer');
+    let html = '';
+    for(i=0; i < products.length; i++) {
+        html += ('<div class="card"><img src = " ' 
+        + products[i].imgUrl + ' "> </img> <p>' 
+        + products[i].name + '</p> <button id="'+ products[i].id +'" onClick="goToDetails()">More</button></div>');
+        container.innerHTML = html;
+      }
+}); 
+
+const goToDetails = (id) => {
+    id = event.srcElement.id;
+    console.log(id)
+    location.href= `details.html?id=${id}`;  
+}
+
+
+
+
+
