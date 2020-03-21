f55674a561b4d85aecac01f3a11b9b4fcd88322c
Adrian Hodrea <adihodrea@yahoo.com>
2020-03-21 19:22:47 +0200

    implemented the mobile styling

--- frontend/controllers/displayProductsList.js
+++ frontend/controllers/displayProductsList.js
@@ -10,16 +10,33 @@ listOfProducts.getProductsList()
 
 function renderProducts(productsContainer, productsList) {
     productsList.forEach(item => {
-        var { name, imgUrl } = item;
-        var card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `
-            <img src=${imgUrl} />
-            <p>${name}</p>
-            <div class="button">More</div>    
-        `
+        var { id, name, imgUrl } = item;
+        var card = document.createElement('div');
+        card.classList.add('card');
+        var img = document.createElement('img');
+        img.setAttribute('src',imgUrl);
+        card.appendChild(img);
+
+        var prodName = document.createElement('p');
+        prodName.innerText = name;
+        card.appendChild(prodName);
+
+        var moreBtn = document.createElement('div');
+        moreBtn.classList.add('button');
+        moreBtn.innerText = 'More';
+        moreBtn.addEventListener('click',onBtnClick());
+        card.appendChild(moreBtn);
+
         productsContainer.appendChild(card);
-    })
+
+        function onBtnClick() {
+            return function() {
+                window.localStorage.setItem("productId",id);
+                console.log("Id conform click:", id);
+                window.open("./productDetails.html","_blank");
+            }
+        }
+    }) // end of forEach
 } 
 
 } // end of function onHtmlLoaded()
