a9bee8804786b8958a57a6c8ba4ec2a639da6c72
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-04-12 17:57:02 +0300

    displaying product text

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,33 @@ const getButtonId = (idVal) => {
   localStorage.setItem("clickedId", JSON.stringify(clickedId));
 };
 
-console.log(JSON.parse(localStorage.getItem("clickedId")), "aici");
+let clickedId = JSON.parse(localStorage.getItem("clickedId"));
+let displayData = JSON.parse(localStorage.getItem("fetchedData"));
+
+displayData.map((el) => {
+  if (el.id == clickedId) {
+    const markup = `
+      <div class="product-details">
+        <div class="product-left">
+          <img id="prod-image" alt="" src="${el.imgUrl}"/>
+        </div>
+        <div class="product-right">
+          <div class="description">
+            <h3 id="prod-title">
+              ${el.name}
+            </h3>
+            <p id="prod-description">
+              ${el.description}
+            </p>
+          </div>
+          <a href="" class="btn"
+            >Add to cart<img
+              src="../img/shopping-cart-solid.svg"
+              style="width: 20px;"
+          /></a>
+        </div>
+      </div>
+`;
+    document.getElementById("mata").innerHTML = markup;
+  }
+});
