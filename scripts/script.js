f3f634d9d182d71c44a786084d47805e792733e3
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-04-12 18:46:57 +0300

    finished make it work procces

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ let displayData = JSON.parse(localStorage.getItem("fetchedData"));
 
 displayData.map((el) => {
   if (el.id == clickedId) {
-    const markup = `
+    const productMarkup = `
       <div class="product-details">
         <div class="product-left">
           <img id="prod-image" alt="" src="${el.imgUrl}"/>
@@ -30,6 +30,33 @@ displayData.map((el) => {
         </div>
       </div>
 `;
-    document.getElementById("mata").innerHTML = markup;
+    for (let i = 0; i < el.comments.length; i++) {
+      const commentsMarkup = `
+      <div class="reviews">
+        <div class="comment">
+          <div class="comment-left">
+            <img
+              id="comm-image"
+              src="${el.comments[i].userImgUrl}"
+              alt=""
+              class="avatar"
+            />
+          </div>
+          <div class="comment-right">
+            <h4 id="comm-user">
+              ${el.comments[i].user}
+            </h4>
+            <p id="comm-text">
+              ${el.comments[i].comment}
+            </p>
+          </div>
+        </div>
+      </div>
+`;
+      let commentNode = document.createElement("div");
+      commentNode.innerHTML = commentsMarkup;
+      document.getElementById("insert-comments").appendChild(commentNode);
+    }
+    document.getElementById("insert-product").innerHTML = productMarkup;
   }
 });
